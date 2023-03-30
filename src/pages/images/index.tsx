78175a51e891b1d71c25b3ee3bd05ccdae71b69e
Heriberto Figueroa <figmiche@gmail.com>
2023-03-29 19:08:17 -0600

    Add Photos to Images page from external API

--- src/pages/images/index.tsx
+++ src/pages/images/index.tsx
@@ -1,7 +1,27 @@
-const images = () => {
+const images = ({ photos }: any) => {
   return (
-    <div>images uwu</div>
+    <div>
+      {
+        photos?.map((photo: any, index: number) => <img src={photo.url} alt={photo.title} key={`user-photo-${photo.album}-${index}`} />)
+      }
+    </div>
   )
+}
+
+export async function getServerSideProps() {
+  try {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/photos`)
+    const photos = await res.json()
+
+    return {
+      props: {
+        photos
+      }
+    }
+  } catch (error) {
+    console.log(error)
+    return{}
+  }
 }
 
 export default images
